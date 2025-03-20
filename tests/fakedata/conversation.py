073a2812ced09d4f484c07da93801946e8f6207d
Alex Vahter <alex.vahter@lhv.ee>
2025-03-20 16:34:10 +0200

    Package updates

--- tests/fakedata/conversation.py
+++ tests/fakedata/conversation.py
@@ -7,7 +7,7 @@ from vocode.streaming.agent.base_agent import BaseAgent
 from vocode.streaming.models.agent import AgentConfig, ChatGPTAgentConfig
 from vocode.streaming.models.audio import AudioEncoding
 from vocode.streaming.models.message import BaseMessage
-#from vocode.streaming.models.synthesizer import PlayHtSynthesizerConfig, SynthesizerConfig
+from vocode.streaming.models.synthesizer import SynthesizerConfig
 from vocode.streaming.models.transcriber import DeepgramTranscriberConfig, TranscriberConfig
 from vocode.streaming.output_device.abstract_output_device import AbstractOutputDevice
 from vocode.streaming.output_device.audio_chunk import ChunkState
